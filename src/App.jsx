306fbaa56c283195bcbe506e50aa786a38e4d217
Arthur Florenço <florencco@gmail.com>
2024-01-20 12:58:58 +0000

    “comentario”

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Calculator from './components/Calculator'
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1>Calculator</h1>
+      <Calculator/>
+      <div className="border" /> 
+      <p>&copy; 2024 </p>
+      <p>Designe by: Arthur Florenco</p>
+     
+    </div>
+  )
+}
+
+export default App
